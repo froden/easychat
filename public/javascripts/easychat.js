c165866a0c947418f15cdeda0940801d28857c8f
Frode Nerbråten <frode@nerbraten.no>
2011-01-14 22:23:49 +0100

    Cleaned up some code

--- public/javascripts/easychat.js
+++ public/javascripts/easychat.js
@@ -9,19 +9,12 @@
 	
 	this.post('#/messages', function(context) {
 		$('#messageform input#message').val("");
-		var message = context.params['message'];
-		context.log(message);
-		$.ajax({
-		  type: 'POST',
-		  url: '/messages',
-		  data: message
-		});
+		$.post('/messages', context.params.message);
 	});
 	
 	this.helper('pollMessages', function() {
-		var context = this;
-		var last = $('#messages').children().last().attr("id") || 0;
-		context.log("last " + last);
+		var context = this,
+		    last = $('#messages').children().last().attr("id") || 0;
 		context.load('messages', {cache: false, data: {last: last}, dataType:'json'}).then(function(messages) {
 	    	$.each(messages, function(i, message) {
 				context.render('public/templates/message.template', {message: message}).appendTo($('#messages'));
@@ -29,7 +22,7 @@
 		}).then(function(messages) {
 			setTimeout(function() {
 				context.pollMessages();
-			}, 500);
+			}, 2000);
 		});
 	});
 
